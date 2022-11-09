d1424e676b3d4efd4fd9a441e44ce61b885e1ec4
FaridPuasa <farid.puasa@globex.com.bn>
2022-11-09 23:57:04 +0800

    update inventory

--- public/js/newinventory.js
+++ public/js/newinventory.js
@@ -11,32 +11,64 @@ val = this.value
 })
 
 function addTextInput() {
-    let test = document.getElementById("newRow")
+    let row = document.getElementById("newRow")
+    
+    let tableRow = document.createElement("tr")
+    let tableCol_2 = document.createElement("td")
+    let tableCol_3 = document.createElement("td")
+    let tableCol_4 = document.createElement("td")
+
+    /*
+    let numbers = document.createElement("input")
+    numbers.type = "hidden"
+    numbers.name = "numbering"
+    numbers.value = "1."
+    */
+    let i = 1
+
+    tableRow.id = "row" + i
+
+    let productColor = document.createElement("input")
+    productColor.type = "text"
+    productColor.className = "form-control"
+    productColor.name = "productColor"
+    productColor.id = "productColor" + i
+    productColor.placeholder = "Enter Product Color"
     
 
-    let div = document.createElement('div');
-    div.className = 'form-group'
-    let div1 = document.createElement('div');
-    div1.className = 'form-group'
+    let productSize = document.createElement("input")
+    productColor.type = "text"
+    productSize.className = "form-control"
+    productSize.name = "productSize"
+    productSize.id = "productSize" + i
+    productSize.placeholder = "Enter Product Size"
     
 
-    let variety = document.createElement("input")
-    variety.className = "form-control"
-    variety.type = "text"
-    variety.name = "variety"
-    variety.id = "variety"
-    variety.placeholder = "Enter Variety"
-
-    let varietyQuantity = document.createElement("input")
-    varietyQuantity.className = "form-control"
-    varietyQuantity.type = "number"
-    varietyQuantity.name = "varietyQuantity"
-    varietyQuantity.id = "varietyQuantity"
-    varietyQuantity.placeholder = "Enter Variety Quantity"
-
-    div.appendChild(variety)
-    div1.appendChild(varietyQuantity)
+    let quantity = document.createElement("input")
+    quantity.type = "text"
+    quantity.className = "form-control"
+    quantity.name = "quantity"
+    quantity.id = "quantity" + i
+    quantity.placeholder = "Enter Quantity"
     
-    test.appendChild(div)
-    test.appendChild(div1)
+
+    tableCol_2.appendChild(productColor)
+    tableCol_3.appendChild(productSize)
+    tableCol_4.appendChild(quantity)
+
+    tableRow.appendChild(tableCol_2)
+    tableRow.appendChild(tableCol_3)
+    tableRow.appendChild(tableCol_4)
+
+    row.appendChild(tableRow)
+    i++
+}
+
+function removeRow(){
+    let i = 1
+    let id = "row" + (i)
+    let row = document.getElementById(id)
+    
+    row.remove()
+    i--
 }
