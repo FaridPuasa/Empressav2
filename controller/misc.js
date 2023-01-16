dc84e708b146ea0292f7816e4b922ec315fe5523
FaridPuasa <farid.puasa@globex.com.bn>
2023-01-17 01:56:46 +0800

    update

--- controller/misc.js
+++ controller/misc.js
@@ -78,7 +78,7 @@ const insertMisc = (req,res)=> {
             console.log('Status: 201 - success entry to database')
             req.flash('success', `${data} has been added to the database.`)
             res.status(201).send()
-            res.redirect('/:services-in')
+            res.redirect('/misc-in')
         }
     })
 }
